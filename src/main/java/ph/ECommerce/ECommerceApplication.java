56ce1d3c746ec348b974bdd8ab6a0eb57d5b648f
Prarthana <phegde015@gmail.com>
2021-09-02 01:27:32 +0530

    Base project for E-commerce

---
+++ src/main/java/ph/ECommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package ph.ECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
