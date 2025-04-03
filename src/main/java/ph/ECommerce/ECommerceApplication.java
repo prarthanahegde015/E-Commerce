6e18a850637c2af13368e310614075dbffc5c388
prarthanahegde015 <phegde015@gmail.com>
2025-04-03 15:37:49 +0530

    Comment added

--- src/main/java/ph/ECommerce/ECommerceApplication.java
+++ src/main/java/ph/ECommerce/ECommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ECommerceApplication {
-
+//comment added
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
